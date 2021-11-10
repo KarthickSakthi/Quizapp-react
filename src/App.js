59d70183352a86ee348a3021036210a3154578d5
KarthickSakthi <karthicksakthi124@gmail.com>
2021-11-10 21:05:17 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from "react";
 import {api} from './Api/api'
 import Quiz from "./components/Quiz/Quiz";
 import style from './App.module.css'
-
+import { Header } from "./components";
 const App = ()=>{
  const [apiData,setApiData] = useState([]);
  const [userName,setUserName]=useState("");
@@ -43,6 +43,9 @@ function handleClick(optionVal,correctAnswer){
 return(
  
   <div className={style.App}>
+   
+    <Header/>
+    <div className={style.AppContainer}>
     {userName ? ( 
       <div className={style.QuizContainer}>
         <div className={style.QuizContainerContent}>
@@ -59,13 +62,16 @@ return(
         </div>
     </div>
     ) : (
-      <div className={style.formInput}>
-      <form onSubmit={userSubmit}>
-      <input type='text' value={userInput} onChange={(e)=>setUserInput(e.target.value)} ></input>
+      <div >
+        <h1>Enter the Require details to Attend Quiz</h1>
+      <form onSubmit={userSubmit} className={style.formInput}>
+        <label>Enter Your Name :</label>
+      <input type='text' value={userInput} onChange={(e)=>setUserInput(e.target.value)} required></input>
       <button type="submit" >Submit</button>
       </form>
       </div>
     )}
+    </div>
   </div>
 )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import style from './Header.module.css'
+
+const Header=()=>{
+  return(
+      <div className={style.Header}>
+       <h1>Quiz App</h1>
+      </div>
+  )
+}
+export default Header
